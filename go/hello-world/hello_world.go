a97dab3687052911d8b2559be5e158174c53ba7f
Adan Gutierrez <hevyw8@gmail.com>
2019-09-02 17:09:17 -0500

    completed first 2 go exercism exercises

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting returns a one line greeting
+package greeting
+
+// HelloWorld returns the string "Hello, world!"
+func HelloWorld() string {
+	return "Hello, World!"
+}
---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,13 @@
+// Package twofer takes a string and inserts it into a string or provides a default value.
+package twofer
+
+import "fmt"
+
+// ShareWith generalizes the "One for you, one for me." string with you if no name is provided.
+func ShareWith(name string) string {
+	if name == "" {
+		return "One for you, one for me."
+	} else {
+		return fmt.Sprintf("One for %s, one for me.", name)
+	}
+}
