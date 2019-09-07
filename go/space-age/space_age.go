f7f4fc214b25db5bcacf17bc2da1399c5c77fc53
Adan Gutierrez <hevyw8@gmail.com>
2019-09-06 21:23:13 -0500

    initial space age solution

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,13 +1,31 @@
-// Package space takes an age in seconds and returns how old someone would be on another planet
+// Package space takes an age in seconds and returns how old someone would be on earth
 package space
 
-var (
-	Planet  string = ""
-	Seconds int
-)
+// Planet string is the variable that will indicate what planet the age would be in earth years
+type Planet string
 
 // Age takes an age in seconds and planet and returns how many earth-years old the person would be.
-func Age(Seconds int) years int {
-	years = Seconds
+func Age(seconds float64, planet Planet) float64 {
+	var years float64
+	earthYearSeconds := 31557600.
+	switch planet {
+	case "Earth":
+		years = seconds / earthYearSeconds
+	case "Mercury":
+		years = seconds / (earthYearSeconds * 0.2408467)
+	case "Venus":
+		years = seconds / (earthYearSeconds * 0.61519726)
+	case "Mars":
+		years = seconds / (earthYearSeconds * 1.8808158)
+	case "Jupiter":
+		years = seconds / (earthYearSeconds * 11.862615)
+	case "Saturn":
+		years = seconds / (earthYearSeconds * 29.447498)
+	case "Uranus":
+		years = seconds / (earthYearSeconds * 84.016846)
+	case "Neptune":
+		years = seconds / (earthYearSeconds * 164.79132)
+	}
+
 	return years
 }
