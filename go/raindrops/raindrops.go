92e627b534b95dad9f9ff721a8aa334af2c4d3c5
Adan Gutierrez <hevyw8@gmail.com>
2019-09-16 17:51:10 -0500

    refactored final raindrops submission

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -7,14 +7,13 @@ func Convert(number int) string {
 	var raindrop string
 	var sounds = map[int]string{3: "Pling", 5: "Plang", 7: "Plong"}
 
-	for k, v := range sounds {
-		if number%k == 0 {
-			raindrop += v
+	for i := 3; i < 8; i += 2 {
+		if number%i == 0 {
+			raindrop += sounds[i]
 		}
 	}
 	if raindrop == "" {
 		raindrop = strconv.Itoa(number)
 	}
-
 	return raindrop
 }
