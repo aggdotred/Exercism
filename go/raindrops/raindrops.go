b70321b3e2e2c7f332494617c67482bb85bbe986
Adan Gutierrez <hevyw8@gmail.com>
2019-09-16 08:35:33 -0500

    refactored raindrops but have intermittent test failures

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -5,20 +5,16 @@ import "strconv"
 // Convert takes an integer and returns a string based on the factors of the integer provided
 func Convert(number int) string {
 	var raindrop string
-	for i := 2; i <= number; i++ {
-		if number%i == 0 {
-			switch i {
-			case 3:
-				raindrop += "Pling"
-			case 5:
-				raindrop += "Plang"
-			case 7:
-				raindrop += "Plong"
-			}
+	var sounds = map[int]string{3: "Pling", 5: "Plang", 7: "Plong"}
+
+	for k, v := range sounds {
+		if number%k == 0 {
+			raindrop += v
 		}
 	}
 	if raindrop == "" {
 		raindrop = strconv.Itoa(number)
 	}
+
 	return raindrop
 }
