2bcb8ef14f54c41a3270f844c29ec4918a718de4
Adan Gutierrez <hevyw8@gmail.com>
2019-09-24 07:04:39 -0500

    second refactor of scrabble

--- go/scrabble-score/scrabble.go
+++ go/scrabble-score/scrabble.go
@@ -3,10 +3,12 @@ package scrabble
 
 import "unicode"
 
+var scrabbleScoreMap = map[rune]int{'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1, 'd': 2, 'g': 2, 'b': 3, 'c': 3, 'm': 3, 'p': 3, 'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4, 'k': 5, 'j': 8, 'x': 8, 'q': 10, 'z': 10}
+
 // Score takes a string and returns the scrabble score as an integer
 func Score(word string) int {
 	score := 0
-	scrabbleScoreMap := map[rune]int{'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1, 'd': 2, 'g': 2, 'b': 3, 'c': 3, 'm': 3, 'p': 3, 'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4, 'k': 5, 'j': 8, 'x': 8, 'q': 10, 'z': 10}
+
 	for _, c := range word {
 		score += scrabbleScoreMap[unicode.ToLower(c)]
 	}
