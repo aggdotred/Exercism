68dacebbb466bf9f746e707d1326cc2206d11f5a
Adan Gutierrez <hevyw8@gmail.com>
2019-10-08 19:38:53 -0500

    rust leap submitted

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    
+    if year % 4 == 0 {
+     if year % 100 == 0 && year % 400 != 0 {
+       return false;
+     } 
+     return true;
+    }
+    return false
 }
